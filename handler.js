38ef40075cd22714f5e19170e43a3a28aa649c8d
unknown <juancarlospenagosnunez@gmail.com>
2025-05-23 20:24:51 -0500

    Borrado handler.js para corregir deploy en Render

--- handler.js
+++
@@ -1,4 +0,0 @@
-const app = require('./App'); // Suponiendo que app.js es tu aplicación Express
-const serverless = require('serverless-http');
-
-module.exports.handler = serverless(app); // Esta es la función que se invoca en AWS Lambda
